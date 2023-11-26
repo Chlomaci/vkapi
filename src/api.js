ab6cd08de258ab418e2607c5ac59a691f02ebcb5
Darina <darinaeme@gmail.com>
2023-11-27 00:53:34 +0300

    commit

---
+++ src/api.js
@@ -0,0 +1,31 @@
+export function loadVKAPI() {
+  return new Promise(function(resolve, reject) {
+    var el = document.createElement("script");
+    el.type = "text/javascript";
+    el.src = "https://vk.com/js/api/openapi.js?169";
+    el.onload = resolve;
+    el.onerror = reject;
+    document.getElementById("vk_api_transport").appendChild(el);
+  });
+}
+
+export function initVKAPI(apiId) {
+  return new Promise(function(resolve, reject) {
+    VK.init({
+      apiId: apiId,
+      status: true,
+      onlyWidgets: true,
+      callback: function() {
+        resolve();
+      }
+    });
+  });
+}
+
+export function callVKAPI(method, params) {
+  return new Promise(function(resolve, reject) {
+    VK.Api.call(method, params, function(response) {
+      resolve(response);
+    });
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 import store, {key} from '@/store';
+import { loadVKAPI, initVKAPI, callVKAPI } from "@/api.js";
+
+loadVKAPI().then(function() {
+  return initVKAPI(51795832);
+}).then(function() {
+  return callVKAPI('friends.get', {});
+}).then(function(response) {
+  console.log(response);
+}).catch(function(error) {
+  console.error("Ошибка при выполнении VK API:", error);
+});
+
 
 const app = createApp(App)
 registerPlugins(app)
